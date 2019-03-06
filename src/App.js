5c3aa6f12817e410cec5ec1fe28e85a02a64e3a4
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2019-03-06 16:46:56 +0600

    Add login page
    Using stub for authentication provider.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
+import authProvider from './authProvider';
+
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
         <Resource name="users" list={ListGuesser} />
     </Admin>
 );
